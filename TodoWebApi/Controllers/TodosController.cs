60094954b2e61b357122388c1fd048ff8d531937
webmasterdevlin <webmasterdevlin@gmail.com>
2018-03-29 16:55:30 +0800

    Add project files.

---
+++ TodoWebApi/Controllers/TodosController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoWebApi.Models;
+
+namespace TodoWebApi.Controllers
+{
+    [Produces("application/xml")]
+    [Route("api/Todos")]
+    public class TodosController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public TodosController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Todos
+        [HttpGet]
+        public IEnumerable<Todo> GetTodo()
+        {
+            return _context.Todo;
+        }
+
+        // GET: api/Todos/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTodo([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var todo = await _context.Todo.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(todo);
+        }
+
+        // PUT: api/Todos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodo([FromRoute] int id, [FromBody] Todo todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != todo.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Todos
+        [HttpPost]
+        public async Task<IActionResult> PostTodo([FromBody] Todo todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Todo.Add(todo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTodo", new { id = todo.Id }, todo);
+        }
+
+        // DELETE: api/Todos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodo([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var todo = await _context.Todo.SingleOrDefaultAsync(m => m.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Todo.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return Ok(todo);
+        }
+
+        private bool TodoExists(int id)
+        {
+            return _context.Todo.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TodoWebApi/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using TodoWebApi.Models;
+
+namespace TodoWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddXmlSerializerFormatters(); // Install Microsoft.AspNetCore.Mvc.Formatters.Xml from Nuget Packages
+
+            services.AddDbContext<TodoContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("TodoContext")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
